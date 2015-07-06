dc23526c95c6562320b87b953156c27757550b4d
nikihowe <nkhrrghw@gmail.com>
2015-07-06 10:41:11 -0500

    sth

--- UserDev/KazuAna/Kazu1/sample.cxx
+++
@@ -1,6 +0,0 @@
-#ifndef KAZUANA_KAZU1_SAMPLE_CXX
-#define KAZUANA_KAZU1_SAMPLE_CXX
-
-#include "sample.h"
-
-#endif
--- core/Analysis/ana_base.cxx
+++
@@ -1,6 +0,0 @@
-#ifndef LARLITE_ANA_BASE_CXX
-#define LARLITE_ANA_BASE_CXX
-
-#include "ana_base.h"
-
-#endif
